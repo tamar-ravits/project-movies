bfeb4346722df468c4a64c313f0f11f535a94c57
tamarso <Tamar.Sompolinsky@amdocs.com>
2022-06-09 01:53:52 +0300

    make it better

--- server/server.js
+++ server/server.js
@@ -3,23 +3,23 @@ var path = require('path');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
+var cors = require('cors')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var moviesRouter = require('./routes/movies');
 
-var port = process.env.PORT || '3000';
-
-server.listen(port);
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(cors())
+app.use(express.static(path.join(__dirname + '/public')));
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/build', 'index.html'));
+//   });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/movies', moviesRouter);
-
-
-
+var port = process.env.PORT || '9000';
+server.listen(port);
 module.exports = app;
